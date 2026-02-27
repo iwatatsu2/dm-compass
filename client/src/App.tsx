b5dc6b2a71b480e92b19b96256adc2846bf5fe39
iwatatsu2 <263831636+iwatatsu2@users.noreply.github.com>
2026-02-27 10:19:20 -0500

    fix: use hash router for GitHub Pages compatibility

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,18 +2,17 @@ import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
+import { useHashLocation } from "wouter/use-hash-location";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import Home from "./pages/Home";
 import CalcPage from "./pages/CalcPage";
-import GuidePage from "./pages/GuidePage";
 import GuidePageEnhanced from "./pages/GuidePageEnhanced";
 import LovableHome from "./pages/LovableHome";
 
 function Router() {
-  // make sure to consider if you need authentication for certain routes
   return (
-    <Switch>
+    <Switch hook={useHashLocation}>
       <Route path="/" component={LovableHome} />
       <Route path="/calc" component={CalcPage} />
       <Route path="/guide" component={GuidePageEnhanced} />
